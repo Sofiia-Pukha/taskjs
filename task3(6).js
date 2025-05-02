0dd3adb0793b30c79aa307c08d2ea5fdf785a13d
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-05-02 14:12:33 +0300

    Create task3(6).js

---
+++ task3(6).js
@@ -0,0 +1,10 @@
+// Реалізуйте програму, яка по натисканню на кнопку видалятиме обраний елемент випадаючого списку. 
+// Можуть видалятися всі елементи в будь-якому порядку.
+// Список з елементами знаходиться у файлі index.html
+// Для запуску використовувати Live Server
+// Для перевірки виконання виконати команду node tests/task3.test.js
+
+document.getElementById('deleteBtn').addEventListener('click', () => {
+  const select = document.getElementById('mySelect');
+  select.remove(select.selectedIndex);
+});
