f947ce09fb1463f94a815e93fbf9134e7ab47cd9
Sofiia-Pukha <sofiia.pukha@gmail.com>
2025-05-02 14:12:02 +0300

    Create task2(6).js

---
+++ task2(6).js
@@ -0,0 +1,19 @@
+// Задано сторінку з 3 кнопками . Напишіть Javascript код і реалізуйте HTML-сторінку з відповідними подіями на кожному елементові:
+//         1) 1-ша кнопка – при кліку на неї колір фону сторінки міняється на синій
+//         2) 2-га кнопка – при подвійному кліку на неї колір фону сторінки міняється на рожевий
+//         3) 3-я кнопка – при наведенні колір фону міняється на коричневий
+//         
+// Запустіть виконане завдання за допомогою Live Server 
+// Перевірте виконання за допомогою команди node tests/task2.test.js
+
+document.getElementById('btn1').addEventListener('click', () => {
+  document.body.style.backgroundColor = 'blue';
+});
+
+document.getElementById('btn2').addEventListener('dblclick', () => {
+  document.body.style.backgroundColor = 'pink';
+});
+
+document.getElementById('btn3').addEventListener('mouseover', () => {
+  document.body.style.backgroundColor = 'brown';
+});
